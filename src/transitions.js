4bc6808abb3356775989f244cfcb3e58409ef628
Sudalai <easylogin2609@gmail.com>
2026-07-06 21:19:08 +0530

    Update transitions.js

--- src/transitions.js
+++ src/transitions.js
@@ -6,3 +6,16 @@ export function updateTransitions() {
     camera.position.x += (15 - camera.position.x) * 0.02;
   }
 }
+
+import { camera, doorway, doorway2 } from './world.js';
+
+export function updateTransitions() {
+  const dist1 = camera.position.distanceTo(doorway.position);
+  if (dist1 < 1) {
+    camera.position.x += (15 - camera.position.x) * 0.02;
+  }
+  const dist2 = camera.position.distanceTo(doorway2.position);
+  if (dist2 < 1) {
+    camera.position.x += (-15 - camera.position.x) * 0.02;
+  }
+}
