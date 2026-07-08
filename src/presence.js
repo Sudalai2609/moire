f45ad6030897bde53e981590e94284976aee1310
Sudalai <easylogin2609@gmail.com>
2026-07-08 21:16:53 +0530

    Update presence.js

--- src/presence.js
+++ src/presence.js
@@ -1,19 +1,14 @@
 import { light } from './world.js';
 import { addMemoryNode } from './memorycorridor.js';
-import { setPresence, listenPresence } from './firebase.js';
-
-// Temporary: each device picks a random-ish name until we add real login
-const myId = localStorage.getItem('moireUserId') || (() => {
-  const id = 'user_' + Math.random().toString(36).slice(2, 8);
-  localStorage.setItem('moireUserId', id);
-  return id;
-})();
+import { setPresence, listenPresence, auth } from './firebase.js';
 
 export let bothPresent = false;
 let lastBoth = false;
 
-setPresence(myId, true);
-addEventListener('beforeunload', () => setPresence(myId, false));
+if (auth.currentUser) {
+  setPresence(auth.currentUser.uid, true);
+  addEventListener('beforeunload', () => setPresence(auth.currentUser.uid, false));
+}
 
 listenPresence(presenceMap => {
   const activeUsers = Object.values(presenceMap).filter(v => v === true).length;
