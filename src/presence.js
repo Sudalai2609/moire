758a7195e61d8e5a7b52fed57d7c68c04808be1c
Sudalai <easylogin2609@gmail.com>
2026-07-08 21:17:17 +0530

    Refactor presence handling with onAuthChange

--- src/presence.js
+++ src/presence.js
@@ -1,14 +1,15 @@
 import { light } from './world.js';
 import { addMemoryNode } from './memorycorridor.js';
-import { setPresence, listenPresence, auth } from './firebase.js';
+import { setPresence, listenPresence, onAuthChange } from './firebase.js';
 
 export let bothPresent = false;
 let lastBoth = false;
 
-if (auth.currentUser) {
-  setPresence(auth.currentUser.uid, true);
-  addEventListener('beforeunload', () => setPresence(auth.currentUser.uid, false));
-}
+onAuthChange(user => {
+  if (!user) return;
+  setPresence(user.uid, true);
+  addEventListener('beforeunload', () => setPresence(user.uid, false));
+});
 
 listenPresence(presenceMap => {
   const activeUsers = Object.values(presenceMap).filter(v => v === true).length;
