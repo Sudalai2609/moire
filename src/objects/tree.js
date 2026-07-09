a0f37b0a3957b8a0694b9d071168e6c5b91f9dea
Sudalai <easylogin2609@gmail.com>
2026-07-09 20:13:55 +0530

    Create tree.js

---
+++ src/objects/tree.js
@@ -0,0 +1,43 @@
+// ======================================================
+// MOIRÉ
+// tree1.js
+// Tree Model Loader
+// ======================================================
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+
+export function createTree1(scene, position = { x: 0, y: 0, z: 0 }) {
+
+    loader.load(
+        "/src/assets/models/tree1.glb",
+
+        (gltf) => {
+
+            const tree = gltf.scene;
+
+            tree.position.set(
+                position.x,
+                position.y,
+                position.z
+            );
+
+            tree.scale.set(
+                1,
+                1,
+                1
+            );
+
+            scene.add(tree);
+
+        },
+
+        undefined,
+
+        (error) => {
+            console.error("Tree1 failed to load:", error);
+        }
+    );
+
+}
