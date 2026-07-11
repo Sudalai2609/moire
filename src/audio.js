f2919a7a9d0bfbaf3c6f7cf1a5cbe0f3738ebaf4
Sudalai <easylogin2609@gmail.com>
2026-07-11 06:20:41 +0530

    Update audio.js

--- src/audio.js
+++ src/audio.js
@@ -3,19 +3,19 @@ let audioCtx, gainNode, osc;
 
 export function initAudio() {
   if (started) return;
+  if (localStorage.getItem('moireSound') === 'off') return;
   started = true;
   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   gainNode = audioCtx.createGain();
-  gainNode.gain.value = 0.02; // very soft
+  gainNode.gain.value = 0.02;
   gainNode.connect(audioCtx.destination);
 
   osc = audioCtx.createOscillator();
   osc.type = 'sine';
-  osc.frequency.value = 110; // low, warm hum
+  osc.frequency.value = 110;
   osc.connect(gainNode);
   osc.start();
 }
 
-// Browsers require a user gesture to start audio
 addEventListener('click', initAudio, { once: true });
 addEventListener('touchstart', initAudio, { once: true });
